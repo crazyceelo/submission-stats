075b87cd411818a76423d47cad496fa7958650ab
Carlos Perez <solracias@gmail.com>
2018-07-31 22:38:24 -0700

    commented the index.js file for later reference

--- crud/src/index.js
+++ crud/src/index.js
@@ -1,15 +1,23 @@
+//react
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// react router
 import { BrowserRouter } from 'react-router-dom';
+
+// create react app package
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// redux
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
+// create the actual store and reducer with dev tools
 const store = createStore(
   rootReducer,
   composeWithDevTools(
@@ -17,6 +25,7 @@ const store = createStore(
   )
 )
 
+// render the entire app at this entry point
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
